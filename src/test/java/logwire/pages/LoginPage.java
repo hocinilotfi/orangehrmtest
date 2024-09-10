9799116158183369416ae50c9be57027f03a8d40
Lotfi Hocini <lotfi.hocini@hotmail.com>
2024-09-10 12:20:28 +0200

    add wait

--- src/test/java/logwire/pages/LoginPage.java
+++ src/test/java/logwire/pages/LoginPage.java
@@ -1,24 +1,28 @@
 package logwire.pages;
+
+import java.time.Duration;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
-
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class LoginPage {
-    WebDriver driver;
+    private WebDriver driver;
+    private WebDriverWait wait;
+
     @FindBy(name = "username")
-    private WebElement identifiantEelment;
+    private WebElement identifiantElement;
     
-    @FindBy(name="password")
+    @FindBy(name = "password")
     private WebElement passwordElement;
 
-    @FindBy(xpath="/html/body/div/div[1]/div[1]/header/div[1]/div[1]/span/h6")
+    @FindBy(xpath = "/html/body/div/div[1]/div[1]/header/div[1]/div[1]/span/h6")
     private WebElement pageSuccess;
 
     @FindBy(xpath = "/html/body/div/div[1]/div/div[1]/div/div[2]/div[2]/form/div[3]/button")
-    // .submit-row > input:nth-child(1)
-    ///html/body/div/div/main/div[1]/div/form/div[3]/input
     private WebElement btnSubmit;
 
     @FindBy(xpath = "/html/body/div/div[1]/div/div[1]/div/div[2]/div[2]/div/div[1]")
@@ -26,29 +30,34 @@ public class LoginPage {
 
     public LoginPage(WebDriver driver) {
         this.driver = driver;
+        this.wait = new WebDriverWait(driver,Duration.ofSeconds(10));
         PageFactory.initElements(driver, this);
     }
 
     public void enterIdentifiant(String identifiant) {
-        identifiantEelment.clear();
-        identifiantEelment.sendKeys(identifiant);
+        wait.until(ExpectedConditions.visibilityOf(identifiantElement));
+        identifiantElement.clear();
+        identifiantElement.sendKeys(identifiant);
     }
+
     public void enterPassword(String password) {
+        wait.until(ExpectedConditions.visibilityOf(passwordElement));
         passwordElement.clear();
         passwordElement.sendKeys(password);
     }
-    public void clickButton(){
+
+    public void clickButton() {
+        wait.until(ExpectedConditions.elementToBeClickable(btnSubmit));
         btnSubmit.click();
     }
 
-     public boolean isSuccessLogin(){
-        // Wait<WebDriver> wait = new WebDriverWait(driver,Duration.ofSeconds(5));
-
+    public boolean isSuccessLogin() {
+        wait.until(ExpectedConditions.visibilityOf(pageSuccess));
         return pageSuccess.isDisplayed();
     }
-    public boolean isErrorMessageDisplayed(){
+
+    public boolean isErrorMessageDisplayed() {
+        wait.until(ExpectedConditions.visibilityOf(errorElement));
         return errorElement.isDisplayed();
     }
-  
-
 }
