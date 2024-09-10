868bdf24cd341f43dcc1cdedcba98628f0009936
Lotfi Hocini <lotfi.hocini@hotmail.com>
2024-09-10 13:04:02 +0200

    repport

--- src/test/java/logwire/RunCucumberTest.java
+++ src/test/java/logwire/RunCucumberTest.java
@@ -11,7 +11,7 @@ import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
 @Suite
 @IncludeEngines("cucumber")
 @SelectClasspathResource("logwire")
-//@ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "pretty")
+@ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "pretty, json:target/cucumber-report/cucumber-report.json")
 // @ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "logwire.stepDefs")
 public class RunCucumberTest {
 }
